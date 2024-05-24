4c33ec1f1fd63ba9bd024575bd649c7ddd92e840
Szewek <shevkpl@gmail.com>
2024-05-24 07:49:32 +0200

    Add more info about file types

--- src/routes/jar/[id=uuid]/filetypes/+page.svelte
+++ src/routes/jar/[id=uuid]/filetypes/+page.svelte
@@ -3,10 +3,11 @@
   let {data}: { data: import('./$types').PageData } = $props()
   let types = $derived(Object.keys(data.sizes))
   const sortFns: ((a: [number, number, number], b: [number, number, number]) => number)[] = [
-    (a, b) => b[0] - a[0],
-    (a, b) => b[1] - a[1],
-    (a, b) => b[2] - a[2],
-    (a, b) => b[2] / b[1] - a[2] / a[1]
+    (a, b) => a[0] - b[0],
+    (a, b) => a[1] - b[1],
+    (a, b) => a[2] - b[2],
+    (a, b) => a[2] / a[1] - b[2] / b[1],
+    (a, b) => (a[2] - a[1]) - (b[2] - b[1]),
   ]
   let sortBy = $state(0)
   let orderAsc = $state(true)
@@ -16,7 +17,7 @@
     if (!orderAsc) as.reverse()
     return as
   })
-  let allSizes = $derived.by(() => {
+  let all = $derived.by(() => {
     let as: [number, number, number] = [0, 0, 0]
     for (const [, j] of Object.entries(data.sizes)) j.forEach((s, i) => as[i] += s)
     return as
@@ -28,28 +29,37 @@
       sortBy = i, orderAsc = true
     }
   }
-  const headNames = ['Type', 'Count', 'Internal size', 'External size', 'Compression ratio']
+  const headNames = ['Type', 'Count', 'Size', 'Compressed', 'Ratio', 'Saving']
 </script>
+{#snippet item(name, [count, size, compressed])}
+  <td>{name}</td>
+  <td>{count}</td>
+  <td>{inKB(size / 1024)}</td>
+  <td>{inKB(compressed / 1024)}</td>
+  <td>{percentFmt(size && compressed / size)}</td>
+  <td>{inKB((size - compressed) / 1024)}</td>
+{/snippet}
 <h1>File types</h1>
 <div>{data.name}</div>
 <table class="border-collapse w-full">
-  <thead>
-    <tr class="b-white/60 b-b-2 b-b-solid *:p-1 hover:*:bg-white/10 select-none">
+  <thead class="b-white/60 b-b-2 b-b-solid">
+    <tr class="*:p-1 hover:*:bg-white/10 select-none">
       {#each headNames as h, i}
-        <th class={sortBy == i ? orderAsc ? 'after:content-["▲"]' : 'after:content-["▼"]' : ''} onclick={() => switchSort(i)}>{h}</th>
+        <th class={sortBy == i ? orderAsc ? 'sort-asc' : 'sort-desc' : ''} onclick={() => switchSort(i)}>{h}</th>
       {/each}
     </tr>
   </thead>
-  <tbody class="text-sm">
-    {#each typesSorted as t}
+  <tbody class="text-sm hover:*:bg-white/10">
+    {#each typesSorted as t (t)}
       {@const d = data.sizes[t]}
-      <tr class="*:p-1 [&>td:not(:first-child)]:text-end hover:bg-white/10 hover:*:bg-white/10">
-        <td>{t == "" ? "<empty>" : t}</td>
-        <td>{d[0]}</td>
-        <td>{inKB(d[1] / 1024)}</td>
-        <td>{inKB(d[2] / 1024)}</td>
-        <td>{percentFmt(d[1] && d[2] / d[1])}</td>
+      <tr class="*:p-1 [&>td:not(:first-child)]:text-end hover:*:bg-white/10">
+        {@render item(t == "" ? "<empty>" : t, d)}
       </tr>
     {/each}
   </tbody>
+  <tfoot class="b-t-2 b-t-solid b-white/40">
+    <tr class="*:p-1 [&>td:not(:first-child)]:text-end hover:bg-white/10 hover:*:bg-white/10">
+      {@render item("Total", all)}
+    </tr>
+  </tfoot>
 </table>
