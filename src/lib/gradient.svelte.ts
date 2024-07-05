ef867fec082b86df5598768aa296f72cfc534413
Szewek <shevkpl@gmail.com>
2024-07-05 06:07:08 +0200

    Use L to calculate new lightness

--- src/lib/gradient.svelte.ts
+++ src/lib/gradient.svelte.ts
@@ -1,4 +1,4 @@
-const htmlImg = (img: HTMLImageElement) => new Promise<Uint8ClampedArray>((ok, err) => {
+const htmlImg = (img: HTMLImageElement) => new Promise<Uint8ClampedArray>(ok => {
   const fn = () => ok(drawImage(img))
   if (img.complete) fn(); else img.onload = fn
 })
@@ -10,11 +10,10 @@ const drawImage = (img: HTMLImageElement) => {
   return ctx.getImageData(0, 0, img.width, img.height).data
 }
 const hex = (n: number) => (n|0).toString(16).padStart(2, '0')
-const grayscale = (r: number, g: number, b: number) => 0.299 * r + 0.587 * g + 0.114 * b
 const rgb2hsl = (r: number, g: number, b: number) => {
   r /= 255, g /= 255, b /= 255
-  const max = Math.max(r, g, b), min = Math.min(r, g, b), d = max - min
-  let h = 0, s = 0, l = (max + min) / 2
+  const max = Math.max(r, g, b), min = Math.min(r, g, b), d = max - min, l = (max + min) / 2
+  let h = 0, s = 0
   if (max !== min) {
     s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
     switch (max) {
@@ -27,25 +26,23 @@ const rgb2hsl = (r: number, g: number, b: number) => {
   return [h, s, l]
 }
 const hsl2rgb = (h: number, s: number, l: number) => {
-  const q = l < 0.5 ? l * (1 + s) : l + s - l * s
-  const p = 2 * l - q
-  const [r, g, b] = [h + 1 / 3, h, h - 1 / 3].map(x => {
+  const q = l < 0.5 ? l * (1 + s) : l + s - l * s, p = 2 * l - q
+  return [h + 1 / 3, h, h - 1 / 3].map(x => {
     if (x < 0) x += 1
     if (x > 1) x -= 1
     if (x < 1 / 6) return p + (q - p) * 6 * x
     if (x < 1 / 2) return q
     if (x < 2 / 3) return p + (q - p) * (2 / 3 - x) * 6
     return p
-  })
-  return [r * 255, g * 255, b * 255]
+  }).map(x => x * 255)
 }
 const findColor = (d: Uint8ClampedArray) => {
   const gap = 40, cols = new Map<string, number>
   for (let i = 0; i < d.length; i += gap) {
     let [r, g, b] = d.subarray(i, i + 3);
-    const [h, s] = rgb2hsl(r, g, b)
+    const [h, s, l] = rgb2hsl(r, g, b)
     if (s < 0.2) continue
-    [r, g, b] = hsl2rgb(h, s, Math.min(Math.max(grayscale(r, g, b), 48), 64) / 255)
+    [r, g, b] = hsl2rgb(h, s, Math.min(Math.max(l, 0.1875), 0.25))
     const c = hex(r) + hex(g) + hex(b)
 
     cols.set(c, (cols.get(c) ?? 0) + 1)
