3769a3fc08cc5202caa32fb0eaa795afe3316011
Szewek <shevkpl@gmail.com>
2024-07-05 04:02:55 +0200

    Improved sort button

--- src/lib/SortBtn.svelte
+++ src/lib/SortBtn.svelte
@@ -2,4 +2,4 @@
   /** @type {{label: string, sort: number}} */
   let {label = "", sort = $bindable()} = $props()
 </script>
-<button class={['', 'sort-desc', 'sort-asc'][sort]} onclick={() => sort = (sort + 1) % 3}>{label}</button>
+<button class={['sort-none', 'sort-desc', 'sort-asc'][sort]} onclick={() => sort = (sort + 1) % 3}>{label}</button>
