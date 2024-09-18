aedbc53e92d9897466da0fb9a8562b158868a2dd
Szewek <shevkpl@gmail.com>
2024-09-18 20:05:37 +0200

    Inline condition in render

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,5 @@
   import Welcome from "$lib/Welcome.svelte"
   import WorkspaceOpen from "$lib/WorkspaceOpen.svelte"
   import { ws } from "$lib/workspace.svelte"
-  let Wsc = ws.isOpen ? WorkspaceOpen : Welcome
 </script>
-<Wsc />
+{#if ws.isOpen} <WorkspaceOpen /> {:else} <Welcome /> {/if}
