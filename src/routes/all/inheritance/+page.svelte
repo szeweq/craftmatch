8f5c577440d23e4fcd32a79b3fc445c817d57451
Szewek <shevkpl@gmail.com>
2024-10-18 06:52:25 +0200

    Hash-based modal dialogs

--- src/routes/all/inheritance/+page.svelte
+++ src/routes/all/inheritance/+page.svelte
@@ -4,6 +4,10 @@
   import SortBtn from '$lib/SortBtn.svelte'
   import { queryable, paginate, sortable } from '$lib/data.svelte'
   let {data}: { data: import('./$types').PageData } = $props()
+  const hashSelect = () => {
+    const hash = location.hash.slice(1)
+    selected = !hash || hash == "#" ? -1 : parseInt(hash)
+  }
   const q = queryable(() => data.indices, x => x[0])
   const sb = sortable(q, ([,j]) => data.inherits[j].length)
   const pag = paginate(sb)
@@ -17,6 +21,7 @@
     if (selected >= 0) dialog?.showModal(); else dialog?.close()
   })
 </script>
+<svelte:window on:hashchange={hashSelect} />
 <div>
   <QInput {...q} />
   <SortBtn label="Sort by count" bind:sort={sb.sortID} />
@@ -24,17 +29,17 @@
 <Paginator {pag} />
 <ul class="text-xs">
   {#each pag as [s, i] (i)}
-    <li><a href="#" onclick={() => selected = i}>{s} ({data.inherits[i].length} inherited classes)</a></li>
+    <li><a href={'#' + i}>{s} ({data.inherits[i].length} inherited classes)</a></li>
   {/each}
 </ul>
 <dialog bind:this={dialog} class="max-w-full">
   {#if selected >= 0}
-  <h2>{selObj.str}</h2>
+  <h3>{selObj.str}</h3>
   <ul class="text-xs">
     {#each selObj.list as c}
       <li>{c}</li>
     {/each}
   </ul>
-  <button onclick={() => selected = -1}>Close</button>
+  <a role="button" href="##">Close</a>
   {/if}
 </dialog>
