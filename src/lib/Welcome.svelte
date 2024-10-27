be7f2f69963a8b9d136c4a20deb387830e6cb94c
Szewek <shevkpl@gmail.com>
2024-10-27 11:16:30 +0100

    Keep dirs in data-val and select dir immediately

--- src/lib/Welcome.svelte
+++ src/lib/Welcome.svelte
@@ -7,8 +7,15 @@
     if (moddirs.length === 0) invoke<string[]>("mod_dirs", {kind: null}).then(x => {
       moddirs = x
     })
-    else if (select) invoke("mod_dirs", {kind: select})
   })
+  const choose = (e: MouseEvent) => {
+    const dir_target = (e.target as HTMLElement).dataset.val
+    if (dir_target) {
+      e.preventDefault()
+      select = dir_target
+      invoke("mod_dirs", {kind: dir_target})
+    }
+  }
 </script>
 <div class="f flex-col items-center justify-center select-none">
   <h1>Welcome!</h1>
@@ -22,9 +29,9 @@
   <p>At: {select}</p>
 {:else if moddirs.length > 0}
   <h2>Found Minecraft directories</h2>
-  <ul class="text-sm b-2 b-solid b-w/40 rounded-md mx-0 my-2 text-truncate">
+  <ul class="text-sm b-2 b-solid b-w/40 rounded-md mx-0 my-2 text-truncate" onclickcapture={choose}>
     {#each moddirs as d (d)}
-      <li><a class="block c-inherit hover:c-inherit! p-1 hov-effect" href="#" onclick={e => {e.preventDefault(); select = d}}>{d}</a></li>
+      <li><a class="block c-inherit hover:c-inherit! p-1 hov-effect" href="#" data-val={d}>{d}</a></li>
     {/each}
   </ul>
 {/if}
