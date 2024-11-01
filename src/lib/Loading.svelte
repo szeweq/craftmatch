c7495de47b896d4b549b1cd755bbda79369c0164
Szewek <shevkpl@gmail.com>
2024-11-01 16:44:03 +0100

    Use consts and optimize loading component

--- src/lib/Loading.svelte
+++ src/lib/Loading.svelte
@@ -1,3 +1,6 @@
+<script module>
+  const dur = {duration: 200}
+</script>
 <script>
   import { navigating } from "$app/stores"
   import { fade } from "svelte/transition"
@@ -7,9 +10,7 @@
   $effect.pre(() => navigating.subscribe(n => load = !!n))
 </script>
 {#if load}
-<div class="fixed inset-0 left-[var(--s-aside)] z-99 darken" in:fade={{duration: 200}} out:fade={{duration: 200}}>
-  <div class="f items-center justify-center text-2xl p-1 m-4">
-    <img src={loadingSvg} alt="Loading" width="32" height="32" />
-  </div>
+<div class="f justify-center fixed inset-0 left-[var(--s-aside)] z-99 darken" in:fade={dur} out:fade={dur}>
+  <img class="m-4" src={loadingSvg} alt="Loading" width="32" height="32" />
 </div>
 {/if}
--- src/lib/SortBtn.svelte
+++ src/lib/SortBtn.svelte
@@ -1,5 +1,8 @@
+<script module>
+  const sorts = ['sort-none', 'sort-desc', 'sort-asc']
+</script>
 <script>
   /** @type {{label: string, sort: number}} */
   let {label = "", sort = $bindable()} = $props()
 </script>
-<button class={['sort-none', 'sort-desc', 'sort-asc'][sort]} onclick={() => sort = (sort + 1) % 3}>{label}</button>
+<button class={sorts[sort]} onclick={() => sort = (sort + 1) % 3}>{label}</button>
